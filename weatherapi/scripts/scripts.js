2ab2ee2ebec3a0e5f782fb41c8f865dbe9584c55
minnie-star <minenhleminnie8@gmail.com>
2025-09-19 19:06:51 +0200

    week 03

---
+++ weatherapi/scripts/scripts.js
@@ -0,0 +1,43 @@
+//SELECT HTML ELEMENTS INT THE DOCUMENT
+const myTown = document.querySelector('#town');
+const myDescription = document.querySelector('#description');
+const myTemperature = document.querySelector('#temperature');
+const myGraphic = document.querySelector('#graphic');
+
+//CREATE REQUIRED VARIABLES FOR THE URL
+const myKey = "e90f84f92a64d1ec43318208404ae35d"
+const myLat = "-29.760751297919757"
+const myLong = "31.050330390772697"
+
+//CONSTRUCT A FULL PATH USING TEMPLATE LITERALS
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
+
+// TRY TO GRAB THE CURRENT WEATHER DATA
+async function apiFetch() {
+  try {
+    const response = await fetch(myURL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+//DISPLAY THE JSON DATA ONTO MY WEB PAGE
+function displayResults(data) {
+    myTown.innerHTML = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML =`${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt', data.weather[0].description)
+}
+
+
+
+apiFetch();
